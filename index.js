bdecb8d2a9b71c264820ce99cab487f4ebf8879c
ShevDenny <sdenny904@gmail.com>
2021-05-22 16:17:40 -0400

    commit tree 828a7b183bb4c716e00516ffa26c795b24104a0d

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+function saturdayFun(skate= "roller-skate") {
+    return `This Saturday, I want to ${skate}!`
+}
+saturdayFun();
+saturdayFun('bathe my dog');
+
+const mondayWork = function(work= "go to the office") {
+    return `This Monday, I will ${work}.`
+}
+mondayWork();
+mondayWork("work from home");
+
+const wrapAdjective = function(random="*") {
+    return function(feels="special") {
+        return `You are ${random}${feels}${random}!`
+    }   
+}
